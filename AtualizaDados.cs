40ebdbe37a709bc49409cfa87a373823d82d001e
James Noleto Brandao <56309987+devjamesbrandao@users.noreply.github.com>
2022-03-04 14:35:28 -0300

    Função para atualizar dados de forma dinâmica
    # Você pode atualizar um ou vários campos na tabela do banco de dados
    # Para isso, basta utilizar uma função lambda

---
+++ AtualizaDados.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Linq.Expressions;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+public class Context : DbContext
+{
+    public Context(){}
+
+    public Context(DbContextOptions<Context> options) : base(options) {}
+}
+
+public class Program
+{   
+    public async Task AtualizaDados<T>(
+        T entidade, params Expression<Func<T, object>>[] camposAlterados
+    )
+    {
+        // Contexto do banco de dados
+        var context = new Context(); 
+
+        var dbEntry = context.Entry(entidade);
+
+        MemberExpression body;
+
+        foreach(var campoAlterado in camposAlterados)
+        {
+            body = campoAlterado.Body as MemberExpression;
+
+            dbEntry.Property(body.Member.Name).IsModified = true;
+        }
+
+        try
+        {
+            await context.SaveChangesAsync();
+        }
+        catch(DbUpdateConcurrencyException) // Evita erros se não conseguir atualizar
+        {
+            return;
+        }
+    }
+}
