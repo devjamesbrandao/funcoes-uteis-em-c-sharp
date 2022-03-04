c4cbe905b996b1ded7193d08079bd7645d1ae15d
James Noleto Brandao <56309987+devjamesbrandao@users.noreply.github.com>
2022-03-04 11:49:26 -0300

    Função para formatar CPF e CNPJ

---
+++ FormataCPFCNPJ.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine(FormatarCPFCNPJ("12345678912"));
+    }
+
+    public static string FormatarCPFCNPJ(string valor)
+    {
+        var valorLimpo = Regex.Replace(valor, "[^0-9]", "");
+
+        string valorFormatado = "";
+
+        if(valorLimpo.Length == 11)
+        {
+            valorFormatado = Convert.ToUInt64(valorLimpo).ToString(@"000\.000\.000\-00");
+        }
+        else if(valorLimpo.Length == 14)
+        {
+            valorFormatado = Convert.ToUInt64(valorLimpo).ToString(@"00\.000\.000\/0000\-00");
+        }
+        else
+        {
+            valorFormatado = "00000000000";
+        }
+
+        return valorFormatado;
+    }
+}
