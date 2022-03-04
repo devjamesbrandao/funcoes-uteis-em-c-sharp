be1e9c9b231cf8165ee8a33a135025257425decd
James Noleto Brandao <56309987+devjamesbrandao@users.noreply.github.com>
2022-03-04 11:46:45 -0300

    Função para remover acento em palavras

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Globalization;
+using System.Text;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine(RemoverAcentos("áúóà"));
+    }
+
+    public static string RemoverAcentos(string texto)
+    {
+        texto = texto.ToUpper();
+
+        string textoNormalizado = texto.Normalize(NormalizationForm.FormKD);
+
+        StringBuilder stringBuilder = new StringBuilder();
+
+        for (int k = 0; k < textoNormalizado.Length; k++)
+        {
+            UnicodeCategory caractereUnicode = CharUnicodeInfo.GetUnicodeCategory(textoNormalizado[k]);
+
+            if (
+                caractereUnicode == UnicodeCategory.SpaceSeparator || 
+                caractereUnicode == UnicodeCategory.UppercaseLetter || 
+                caractereUnicode == UnicodeCategory.DecimalDigitNumber
+            )
+            {
+                stringBuilder.Append(textoNormalizado[k]);
+            }
+        }
+
+        return stringBuilder.ToString();
+    }
+}
