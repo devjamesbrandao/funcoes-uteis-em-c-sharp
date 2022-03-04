09bc3b15d812c203a0d1bbcf9ecd315149fc6be3
James Noleto Brandao <56309987+devjamesbrandao@users.noreply.github.com>
2022-03-04 12:04:09 -0300

    POST e Basic Authentication em C#
    # A função utiliza HttpClient
    # Realiza requisições do tipo POST com BASIC AUTHENTICATION
    # É necessário instalar o pacote Newtonsoft.Json

---
+++ PostAsync.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+public class Program
+{
+    private static readonly HttpClient _client = new HttpClient();
+    
+    public async static Task Main(string[] args)
+    {
+        await PostAsyncGenerico();
+    }
+
+    public static async Task<bool> PostAsyncGenerico()
+    {
+        try
+        {
+            // Cabeçalho
+            _client.DefaultRequestHeaders.TryAddWithoutValidation("Content-Type", "application/json");
+
+            var dominio = "http://dominio.com.br/api/cadastrar";
+
+            var autenticacao = $"usuario:senha";
+
+            var base64Autenticacao = Convert.ToBase64String(
+                System.Text.ASCIIEncoding.UTF8.GetBytes(autenticacao)
+            );
+
+            _client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue(
+                "Basic", base64Autenticacao
+            );
+
+
+            // Body
+            var parametros = JsonConvert.SerializeObject("objeto para serializar");
+
+            var buffer = System.Text.Encoding.UTF8.GetBytes(parametros);
+
+            var byteContent = new ByteArrayContent(buffer);
+
+            byteContent.Headers.ContentType = new MediaTypeHeaderValue("application/json");
+
+
+            // Requisição e resposta
+            var Resposta = await _client.PostAsync(dominio, byteContent);
+
+            Resposta.EnsureSuccessStatusCode();
+
+            var JSONResposta = await Resposta.Content.ReadAsStringAsync();
+
+            return await Task.FromResult(true);
+        }
+        catch(Exception)
+        {
+            return await Task.FromResult(false);
+        }
+    }
+}
