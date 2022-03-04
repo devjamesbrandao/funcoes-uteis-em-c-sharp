7766f69c8d87fecf1f05c3b86b2ad37dcb85c51e
James Noleto Brandao <56309987+devjamesbrandao@users.noreply.github.com>
2022-03-04 11:51:01 -0300

    Função para arredondar casas decimais

---
+++ ArredondaCasasDecimais.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine(ArredondarCasasDecimais(12.3333M, 2));
+    }
+
+    public static decimal ArredondarCasasDecimais(decimal valor, double casasDecimais)
+    {
+        var potencia = Convert.ToDecimal(Math.Pow(10, casasDecimais));
+        
+        return Math.Floor(valor * potencia) / potencia;
+    }
+}
